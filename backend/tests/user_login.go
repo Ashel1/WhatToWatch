06272bb5d94540fe5582f37c097dd2cc4db61f1a
Abhishek Redwal <79399990+abhishekredwal@users.noreply.github.com>
2022-04-01 23:39:26 -0400

    add files via upload

---
+++ backend/tests/user_login.go
@@ -0,0 +1,40 @@
+package test
+
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	// here!
+)
+
+//login and register
+
+//test register
+func TestRegister(t *testing.T) {
+	g := gin.Default()
+	SetUpRoutes(g)
+	//registration
+	var jsonStr = []byte(`{"Firstname":"abc","Lastname":"def","username":"abhi","email":"xyz@pqr.com","password":"11aabbcc"}`)
+
+	req, err := http.NewRequest("POST", "/api/register", bytes.NewBuffer(jsonStr)) //router.POST("/api/register", users.Register)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(Register)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+	//check
+	expected := `{"Firstname":"abc","Lastname":"def","username":"abhi","email":"xyz@pqr.com","password":"11aabbcc"}`
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
