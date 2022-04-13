57e4f75c954c412229e8268255e56ab141fc1c3e
Anupama Nair <nairanu98@gmail.com>
2022-04-13 16:17:48 -0400

    watchlist page

--- frontend/src/app/watch-list/watch-list.component.ts
+++ frontend/src/app/watch-list/watch-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Movie } from '/Users/anupamanair/Desktop/sprint4/WhatToWatch/frontend/src/shared/movie';
 
 @Component({
   selector: 'app-watch-list',
@@ -11,5 +12,32 @@ export class WatchListComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  movies: Movie[] = [
+    {
+      id: '0',
+      name: 'Hamilton',
+      image:'https://m.media-amazon.com/images/M/MV5BNjViNWRjYWEtZTI0NC00N2E3LTk0NGQtMjY4NTM3OGNkZjY0XkEyXkFqcGdeQXVyMjUxMTY3ODM@._V1_UX67_CR0,0,67,98_AL_.jpg',
+      // tslint:disable-next-line:max-line-length
+      description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
+    },
+    {
+      id: '1',
+      name: 'Alice Faye',
+      image:"./../assets/images/8th.jpeg",     
+      description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
+    },
+    {
+      id: '2',
+      name: 'The Dark Knight',
+      image:"./../assets/images/9th.jpeg",     
+      description: 'A quintessential ConFusion experience, is it a vada or is it a donut?'
+    },
+    {
+      id: '3',
+      name: 'Kill Bill',
+      image:"./../assets/images/10th.jpeg",     
+      description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
+    }
+   ];
 
 }
---
+++ frontend/src/shared/movie.ts
@@ -0,0 +1,6 @@
+export class Movie {
+    id: string | undefined;
+    name!: string;
+    image: string | undefined;
+    description: string | undefined;
+}
