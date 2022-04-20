e6600b1510d27f61a333881d3dc29f7efb53831c
Ashutosh Yadav <ash.y.012000@gmail.com>
2022-04-20 19:56:00 -0400

    Host changes

--- frontend/server.js
+++ frontend/server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-app-heroku'));
+app.use(express.static('./dist/frontend'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
+    res.sendFile('index.html', {root: 'dist/frontend/'}),
 );
 
 // Start the app by listening on the default Heroku port
